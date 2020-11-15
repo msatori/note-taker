1c9f0c88ba6afd9683189a43c1138a3ee1e1e7a4
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-11-15 17:08:29 -0600

    switching branches

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,11 +12,24 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
   //look at module project for setting an id
-
+  let result = findById(req.params.id, notes);
+  if(result) {
+    res.json(result);
+  } else {
+    res.send(404)
+  }
   //validate data
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+ 
 });
 
 //use note id to delete note
+router.delete('/notes:id', (req, res) => {
+  let result = findById(req.params.id, notes);
+  
+  .removeNote(req.params.id)
+  .then(() => res.json({ok: true}))
+  .catch((err) => res.status(404).json(err))
+
+})
 
 module.exports = router;
