7fcf4ce1cedac4e55dcb473a5ab7021656dff0d0
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-11-16 11:25:20 -0600

    notes populating

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,16 +2,30 @@ const db = require('../db/db.json');
 const fs = require('fs');
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+const { route } = require('./htmlRoutes');
 
 
 router.get('/notes', (req, res) => {
+
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    //parse data from db array
+    const noteData = JSON.parse(data);
+    //push the new note into db array
+    res.json(noteData)
+  });
+});
+
+router.post('/notes', (req, res) => {
+  //look at module project for setting an id
   let noteId = uuidv4();
 
-    let newNote = {
-      id: noteId,
-      title: req.body.title,
-      text: req.body.text
-    };
+  let newNote = {
+    id: noteId,
+    title: req.body.title,
+    text: req.body.text
+  };
+
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     //parse data from db array
@@ -19,38 +33,26 @@ router.get('/notes', (req, res) => {
     //push the new note into db array
     noteArr.push(newNote);
 
-    res.json(db)
-  });
+    fs.writeFile('./db/db.json', JSON.stringify(noteArr, null, 2), err => {
+      if (err) throw new err;
+
+      res.send(db);
+
+      console.log('New Note Created!')
+      console.log(noteArr);
+    })
+  })
 });
 
-  router.post('/notes', (req, res) => {
-    //look at module project for setting an id
-    let noteId = uuidv4();
+route.delete('/notes/:id', (req, res) => {
+  noteData = fs.readFileSync('./db/db.json','utf8');
 
-    let newNote = {
-      id: noteId,
-      title: req.body.title,
-      text: req.body.text
-    };
+  noteData = JSON.parse(noteData);
 
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      //parse data from db array
-      const noteArr = JSON.parse(data);
-      //push the new note into db array
-      noteArr.push(newNote);
+  noteData = noteData.filter(function(note) {
+    return noteData.id  
+  })
 
-      fs.writeFile('./db/db.json', JSON.stringify(noteArr, null, 2), err => {
-        if (err) throw new err;
+})
 
-        res.send(db);
-
-        console.log('New Note Created!')
-        console.log(noteArr);
-      })
-    })
-  });
-
-
-
-  module.exports = router;
+module.exports = router;
