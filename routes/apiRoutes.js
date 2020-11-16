86edf533ee43c7ed16c59cf270f96914e9418789
Maya Santiago <69778090+msatori@users.noreply.github.com>
2020-11-16 09:31:21 -0600

    build out get route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,41 +3,54 @@ const fs = require('fs');
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 
-//test
 
-//create route to send back a html file 
 router.get('/notes', (req, res) => {
-  res.send(db)
-});
-
-router.post('/notes', (req, res) => {
-  //look at module project for setting an id
   let noteId = uuidv4();
 
-  let newNote = {
-    id: noteId,
-    title: req.body.title,
-    text: req.body.text
-  };
-
+    let newNote = {
+      id: noteId,
+      title: req.body.title,
+      text: req.body.text
+    };
   fs.readFile('./db/db.json', 'utf8', (err, data) => {
     if (err) throw err;
     //parse data from db array
     const noteArr = JSON.parse(data);
     //push the new note into db array
     noteArr.push(newNote);
-    
-    fs.writeFile('./db/db.json', JSON.stringify(noteArr, null, 2), err => {
-      if (err) throw new err;
 
-      res.send(db);
-
-      console.log('New Note Created!')
-      console.log(noteArr);
-    })
-  })
+    res.json(db)
+  });
 });
 
+  router.post('/notes', (req, res) => {
+    //look at module project for setting an id
+    let noteId = uuidv4();
+
+    let newNote = {
+      id: noteId,
+      title: req.body.title,
+      text: req.body.text
+    };
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) throw err;
+      //parse data from db array
+      const noteArr = JSON.parse(data);
+      //push the new note into db array
+      noteArr.push(newNote);
+
+      fs.writeFile('./db/db.json', JSON.stringify(noteArr, null, 2), err => {
+        if (err) throw new err;
+
+        res.send(db);
+
+        console.log('New Note Created!')
+        console.log(noteArr);
+      })
+    })
+  });
 
 
-module.exports = router;
+
+  module.exports = router;
